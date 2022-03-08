8bcd3f7edbf3ee958b18e490bbe6b7d40e549581
bnl75 <diegoartu@hotmail.com>
2022-03-08 02:35:52 -0600

    Inicio banner

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'azul-oscuro': 'rgb(38, 61, 110)',
+      }
+    },
+  },
+  plugins: [],
+}
